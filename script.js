9bf8fad4f78fae8db22f5c909484dd3149589e93
AngunJL <minecraftidjele@gmail.com>
2025-03-09 07:52:06 +0000

    DAY 4 js complete

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function Login(){
-   alert("Login success!");
-}
+function Login() {
+   var user = document.getElementById("username").value;
+   var pass = document.getElementById("password").value;
+   if(user == "test" && pass == "password") {
+       alert("Logged In");
+       return false;
+   } else {
+       alert("wrong user/pass");
+       return false;
+       }
+   }
+
 
 function Signup(){
-    alert("Signup success!");
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    var repass = document.getElementById("repassword").value;
+   if(pass == repass){
+      alert("Signup success");
+   }
+   else {
+      alert("Passwords do not match");
+   }
  }
 
- 
